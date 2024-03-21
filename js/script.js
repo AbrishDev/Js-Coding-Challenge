10b920a2c0c57b8cd00c97d33e44afa18b63008d
abrham-desalegn <abrhamdesalegn961@gmail.com>
2024-03-21 13:59:50 +0300

    commit on nignen challenge

--- js/script.js
+++ js/script.js
@@ -184,3 +184,14 @@ console.log(totals);
 console.log(calcAverage(totals));
 
 // Nigneth coding challenge
+const printForecast = function(arr) {
+    let str = `...`;
+    for (let i = 0; i < arr.length; i++) {      
+      str += ` ${arr[i]}^C in ${i + 1} days ...`;
+    }
+    console.log(str);
+}
+
+printForecast([17,21,23]);
+printForecast([12,5,-5,0,4]);
+
